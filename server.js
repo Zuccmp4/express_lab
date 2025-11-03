9718c63be0918125ff8e7f24414a97a77e7c45b2
Zuccmp4 <aeira49@gmail.com>
2025-11-03 14:10:55 -0500

    Added updated routes file

--- server.js
+++ server.js
@@ -1,19 +1,15 @@
 const express = require('express');
-
+const userRouter = require ('./routes/users');
 const app = express(); //Calling this function sets up a server
 app.set('view engine', 'ejs');
+app.use('/users', userRouter);
+
 
 app.get('/', (req, res)=>{
     console.log('Here');
     res.render("index", {user: "Anthony"});
 }); //This function will run when someone when someone goes to the root folder
 
-app.get('/users', (req, res)=>{
-    res.send('User List');
-});
-app.get('users/new', (req,res) =>{
-    res.send('New User Form');
-});
 
 app.listen(3030);
 
