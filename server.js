2ef533cabf29cf2f7d839a0ccb8e0fd0d2e984cd
Zuccmp4 <aeira49@gmail.com>
2025-11-03 13:40:42 -0500

    Added EJS

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
+
 const app = express(); //Calling this function sets up a server
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res)=>{
     console.log('Here');
-    res.send("Hello World!");
+    res.render("index");
 }); //This function will run when someone when someone goes to the root folder
 
 
 app.listen(3030);
+
