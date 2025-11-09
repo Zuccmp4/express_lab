5c79cedc556edbad2d17adac6ae3cd90e5e7ed5f
Zuccmp4 <aeira49@gmail.com>
2025-11-09 16:00:25 -0500

    removed a hw reminder for myself, and added in posts Router and /posts to the server

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const userRouter = require ('./routes/users');
+const postRouter = require('./routes/posts');
 const app = express(); //Calling this function sets up a server
 app.set('view engine', 'ejs');
 app.use('/users', userRouter);
+app.use('/posts', postRouter);
 
-
-//MAKE YOUR OWN ROUTER FOR A FILE CALLED POSTS
 
 app.get('/', (req, res)=>{
     console.log('Here');
