53389f7bd9969b2944bde1cf4c4441c625fdc9ac
Zuccmp4 <aeira49@gmail.com>
2025-11-09 16:04:08 -0500

    Updated ejs for Posts page, and updated name of page title in posts.js

--- routes/posts.js
+++ routes/posts.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('Posts');
+    res.send('Posts List');
 });
 router.get('/new', (req, res) => {
     res.send('New Post Form');
