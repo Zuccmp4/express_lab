f1dcea8b6401f1ffebde6c9bc33144ae94e6430c
Zuccmp4 <aeira49@gmail.com>
2025-11-12 14:00:01 -0500

    Added id param function with callback

--- routes/users.js
+++ routes/users.js
@@ -9,7 +9,7 @@ router.get('/new', (req, res) =>{
     res.send('New User Form');
 });
 
-router.route(":/id").get('/:id', (req, res) => {
+router.route(":/id").get((req, res) => {
     res.send(`Getting User data: ${req.params.id}`);
 }).delete((req, res) => {
     res.send(`Deleting user with id: ${req.params.id}`);
@@ -21,6 +21,9 @@ router.post('/', (req, res) => {
 
 });
 
-
+router.param("id", (req, res, next, id)=>{
+    console.log(`Accessing user #${id}`);
+    next();
+});
 
 module.exports = router;
